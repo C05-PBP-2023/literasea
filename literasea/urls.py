60b5d8b23dd12dd389701ad250b194314d41fcd2
nabilmuafa <nmuafa@gmail.com>
2023-10-11 12:04:28 +0700

    configure static, whitenoise

--- literasea/urls.py
+++ literasea/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('auth/', include('auth.urls')),
+    path('auth/', include('authentication.urls')),
     path('products/', include('products.urls')),
     path('wishlist/', include('wishlist.urls')),
     path('forum/', include('forum.urls')),
