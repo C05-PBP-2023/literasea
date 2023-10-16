4e95f03c3c431dea8aebc984af0cdb01da481c9d
nabilmuafa <nmuafa@gmail.com>
2023-10-16 14:10:02 +0700

    remove email field from UserProfile

--- authentication/models.py
+++ authentication/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.charField(max_length=100)
-    email = models.emailField(max_length=100)
     user_type = models.charField(max_length=10)
