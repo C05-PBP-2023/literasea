cb0ccc4c592e341bcaee8ade72fff5beabb19e35
nabilmuafa <nmuafa@gmail.com>
2023-10-16 14:33:54 +0700

    fix model field

--- authentication/models.py
+++ authentication/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.charField(max_length=100)
-    user_type = models.charField(max_length=10)
+    full_name = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=10)
