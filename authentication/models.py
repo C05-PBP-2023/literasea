6151b4f99463dc7ee13268ed8002c1d5ea7a8e7d
nabilmuafa <nmuafa@gmail.com>
2023-10-16 12:49:29 +0700

    add UserProfile model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.charField(max_length=100)
+    email = models.emailField(max_length=100)
+    user_type = models.charField(max_length=10)
