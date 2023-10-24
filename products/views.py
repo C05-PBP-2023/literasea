ac49f0f1778651daefc2514f1c0763367ae4bee3
Lim Bodhi Wijaya <lim.bodhi@ui.ac.id>
2023-10-25 00:36:30 +0700

    Selesai implementasi add to cart

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Katalog
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from authentication.models import UserProfile
+from django.shortcuts import redirect
 
 # Create your views here.
 def show_katalog(request):
@@ -16,3 +18,9 @@ def book_detail(request, book_id):
     book = get_object_or_404(Katalog, id=book_id)
     context = {'book': book}
     return render(request, 'book_detail.html', context)
+
+def add_book(request, book_id, user_id):
+    user = get_object_or_404(UserProfile, id=user_id)
+    book = get_object_or_404(Katalog, id=book_id)
+    user.cart.add(book)
+    return redirect('products:show_katalog')
