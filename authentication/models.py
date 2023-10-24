1703f6eb3db37005016d2066b53e7a64b29089de
nabilmuafa <nmuafa@gmail.com>
2023-10-24 21:01:52 +0700

    add cart field to UserProfile

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from products.models import Katalog
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100)
     user_type = models.CharField(max_length=10)
+    cart = models.ManyToManyField(Katalog)
