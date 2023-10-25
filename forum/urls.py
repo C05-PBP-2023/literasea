3ce169dbbb46a9dcebd999bc849a42cfc2438f1d
nabilmuafa <nmuafa@gmail.com>
2023-10-25 14:24:41 +0700

    add book choosing page

--- forum/urls.py
+++ forum/urls.py
@@ -4,5 +4,6 @@ from forum.views import *
 app_name = 'forum'
 
 urlpatterns = [
-    path("", show_main, name="forum")
+    path('', show_main, name='forum'),
+    path('choose-book/', choose_book, name="choose_book")
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from authentication.models import UserProfile
+from products.models import Katalog
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url="authentication:login")
 def show_main(request):
     return render(request, 'qna.html', {})
+
+@login_required(login_url="authentication:login")
+def choose_book(request):
+    books = Katalog.objects.all()
+
+    context = {
+        'products': books,
+    }
+
+    return render(request, "katalog_choose.html", context)
+
+@login_required(login_url="authentication:login")
+def write_question(request):
+    pass
