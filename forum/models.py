5e7ad4b05964a1bdd9a2fc6c324c80fe5193fe35
nabilmuafa <nmuafa@gmail.com>
2023-10-25 13:42:58 +0700

    create model, page layout

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    user_ask = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_ask")
+    user_answer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_answer")
+    question = models.TextField()
+    answer = models.TextField()
