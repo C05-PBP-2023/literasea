0b0ab421395f50c0810b7645020ef6b9e3cc9bcb
nabilmuafa <nmuafa@gmail.com>
2023-10-25 12:10:10 +0700

    initial forum page

--- forum/urls.py
+++ forum/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from forum.views import *
 
 app_name = 'forum'
 
 urlpatterns = [
+    path("", show_main, name="forum")
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    return render(request, 'qna.html', {})
