7d22d4b6747ec30b813ec2bc6827bf47c1cb0a01
nabilmuafa <nmuafa@gmail.com>
2023-10-25 11:49:58 +0700

    fix login dependency error

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from authentication.models import UserProfile
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url="{% url 'main:login: }")
+@login_required(login_url="authentication:login")
 def show_katalog(request):
     books = Katalog.objects.all()
 
@@ -15,13 +15,13 @@ def show_katalog(request):
 
     return render(request, "katalog.html", context)
 
-@login_required(login_url="{% url 'main:login: }")
+@login_required(login_url="authentication:login")
 def book_detail(request, book_id):
     book = get_object_or_404(Katalog, id=book_id)
     context = {'book': book}
     return render(request, 'book_detail.html', context)
 
-@login_required(login_url="{% url 'main:login: }")
+@login_required(login_url="authentication:login")
 def add_book(request, book_id, user_id):
     user = get_object_or_404(UserProfile, id=user_id)
     book = get_object_or_404(Katalog, id=book_id)
