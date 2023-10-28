521718e111efeaf5e106548df602bd2853b362fd
ariana <ariana.nurlayla@ui.ac.id>
2023-10-29 06:45:43 +0700

    feat(tracker): update AJAX

--- tracker/static/tracker.js
+++ tracker/static/tracker.js
@@ -3,13 +3,20 @@ $(document).ready(async function () {
         event.preventDefault();
         var form_data = $(this).serialize();
         try {
-            const response = await fetch("{% url 'tracker:add_tracked' %}", {
+            const response = await fetch("{% url 'tracker:add_tracked_ajax' %}", {
                 method: "POST",
                 body: form_data,
             });
             const data = await response.json();
-            // Refresh show_tracked.html content
-            $("#tracked-content").load("{% url 'tracker:show_tracked' %} #tracked-content");
+
+            if (data.status === 'success') {
+                // Berhasil update tracker
+                console.log(data.message);
+                // Refresh show_tracked.html content
+                $("#tracked-content").load("{% url 'tracker:show_tracked' %} #tracked-content");
+            } else {
+                console.error(data.message);
+            }
         } catch (error) {
             console.error(error);
         }
