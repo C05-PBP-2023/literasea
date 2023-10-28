38355f5b5722d5cc16c632ec8fe917fb38bafd59
Ethanic <mmfathan5@gmail.com>
2023-10-29 05:21:32 +0700

    export font di tailwind config and import font inter di head

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        backgroundSize: {
+            'sm': '1.5em'
+        },
+        extend: {
+            fontFamily: {
+                gabarito: ["Gabarito", "sans-serif"],
+                raleway: ["Raleway", "sans-serif"],
+                inter: ["Inter", "sans-serif"]
+            },
+        },
+    },
+    plugins: [],
+}
