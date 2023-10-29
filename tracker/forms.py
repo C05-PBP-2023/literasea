1c7844e1ae222f1a6b2c90ddee7efac8d60e24d9
ariana <ariana.nurlayla@ui.ac.id>
2023-10-29 21:16:50 +0700

    refactor(tracker): no significant changes

--- tracker/forms.py
+++ tracker/forms.py
@@ -2,12 +2,15 @@ from django import forms
 from django.forms import ModelForm
 from tracker.models import BookTracker
 
+
 class addTrackerForm(ModelForm):
     class Meta:
         model = BookTracker
-        fields = ['judul', 'halaman_terakhir']
+        fields = ["judul", "halaman_terakhir"]
 
         widgets = {
-            'judul': forms.Select(attrs={'class': 'w-full p-2 border rounded'}),
-            'halaman_terakhir': forms.NumberInput(attrs={'class': 'w-full p-2 border rounded'}),
+            "judul": forms.Select(attrs={"class": "w-full p-2 border rounded"}),
+            "halaman_terakhir": forms.NumberInput(
+                attrs={"class": "w-full p-2 border rounded"}
+            ),
         }
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import *
 
-app_name = 'tracker'
+app_name = "tracker"
 
 urlpatterns = [
     path("", show_tracked, name="show_tracked"),
-    path("addtracked/", add_tracked, name="add_tracked"),  # Tanpa AJAX
-    path("addtracked/ajax/", add_tracked_ajax, name="add_tracked_ajax"),  # Dengan AJAX
+    path("addtracked/", add_tracked, name="add_tracked"),
+    path("addtracked/ajax/", add_tracked_ajax, name="add_tracked_ajax"),
 ]
