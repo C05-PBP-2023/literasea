5053d33a8110adfd3b39c06512b8dede95c204a2
codaaa19 <tsabitiphone@gmail.com>
2023-12-18 15:35:31 +0700

    Still try to fix the latest problem

--- review/views.py
+++ review/views.py
@@ -125,9 +125,19 @@ def show_random_book_flutter(request):
 @csrf_exempt
 def show_bookUser_flutter(request):
     data = []
+    counter = 1
     data1 = Katalog.objects.get(pk=(request.user.pk+1)%100)
     data2 = Katalog.objects.get(pk=(request.user.pk+2)%100)
     data3 = Katalog.objects.get(pk=(request.user.pk+3)%100)
+    if (data1.id == 0):
+        data1 = Katalog.objects.get(pk=(request.user.pk+1+counter)%100)
+        data2 = Katalog.objects.get(pk=(request.user.pk+2+counter)%100)
+        data3 = Katalog.objects.get(pk=(request.user.pk+3+counter)%100)
+    elif (data2.id == 0):
+        data2 = Katalog.objects.get(pk=(request.user.pk+2+counter)%100)
+        data3 = Katalog.objects.get(pk=(request.user.pk+3+counter)%100)
+    elif (data3.id == 0):
+        data3 = Katalog.objects.get(pk=(request.user.pk+3+counter)%100)
     tmp = []
     tmp.append(data1)
     tmp.append(data2)
